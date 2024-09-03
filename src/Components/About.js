1c249b29ede5114e180cc7187d4bcbb7c4402fcd
BhandavyaBv <bh626198@dal.ca>
2024-09-03 17:32:04 -0300

    Updated protfolio

--- src/Components/About.js
+++ src/Components/About.js
@@ -14,6 +14,8 @@ function About(){
                     <p style={{textAlign:"justify"}}>As a graduate student, I am actively engaged in acquiring expertise in cutting-edge technologies, while also focusing on mastering development best practices. This dual emphasis equips me with the skills needed to contribute innovatively in the rapidly evolving field of technology. </p>
                     
                     <p className='font-120 text-white mb-0 fw-bold'>Experience</p>
+                    <p className='mb-0'>Software Developer Intern, <span style={{fontSize:"80%"}}><i>May 2024 - Aug 2024</i></span></p>
+                    <p className=''>Dash Hudson, Halifax, Canada</p>
                     <p className='mb-0'>Associate Software Developer, <span style={{fontSize:"80%"}}><i>Aug 2019 - Mar 2023</i></span></p>
                     <p className=''>Maersk GSC, Bangalore</p>
                     <p style={{textAlign:"justify"}}>
@@ -34,8 +36,7 @@ function About(){
                     <div className='card' style={{backgroundColor:"#e3e3e3"}}>
                         <div className='card-body'>
                             <p className='font-120 fw-bold'>Languages </p>
-                           
-                            <img src="https://www.vectorlogo.zone/logos/java/java-horizontal.svg" className='pe-2'/>
+                            <img src="https://www.vectorlogo.zone/logos/dotnet/dotnet-ar21.svg" className='pe-2'/>
                             <img src="https://www.vectorlogo.zone/logos/python/python-ar21.svg" className='pe-3'/>
                             <img src="https://www.vectorlogo.zone/logos/javascript/javascript-ar21.svg" className='pe-2'/>
                             <img src="https://www.vectorlogo.zone/logos/w3_html5/w3_html5-ar21.svg" className='pe-3'/>
@@ -49,9 +50,9 @@ function About(){
                             <p className='font-120 fw-bold'>Frameworks</p>
 
                             <img src="https://www.vectorlogo.zone/logos/nodejs/nodejs-ar21.svg" className='pe-3'/>
-                            <img src="https://www.vectorlogo.zone/logos/springio/springio-ar21.svg" className='pe-3'/>
                             <img src="https://www.vectorlogo.zone/logos/pocoo_flask/pocoo_flask-ar21.svg" className='pe-3'/>
                             <img src="https://www.vectorlogo.zone/logos/reactjs/reactjs-ar21.svg" className='pe-3'/>
+                            <img src="https://www.vectorlogo.zone/logos/vuejs/vuejs-ar21.svg" className='pe-3'/>
                             <img src="https://www.vectorlogo.zone/logos/angular/angular-ar21.svg" className='pe-3'/>
                             <img src="https://www.vectorlogo.zone/logos/getbootstrap/getbootstrap-ar21.svg" className='pe-3'/>
                         </div>
--- src/Components/NavbarComponent.js
+++ src/Components/NavbarComponent.js
@@ -24,7 +24,7 @@ class Navbar extends Component{
                 <Link to="/contact" className="nav-link font-120 white pointer px-4" >Contact</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link font-120 white pointer" href='https://drive.google.com/file/d/1wvK4_8SiMAxW3jVcf0tJdg1Tsoa1kTbr/view?usp=sharing' target='_blank'>Resume</a>
+                <a className="nav-link font-120 white pointer" href='https://drive.google.com/file/d/1B79GlUVvjcCB30dDslvToyknq57w0DRt/view?usp=drive_link' target='_blank'>Resume</a>
               </li>
             </ul>
           </div>
